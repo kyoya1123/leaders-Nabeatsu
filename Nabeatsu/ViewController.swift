94bfe4f8b68686402ff1a0b2f0c027b797886ce2
Kyoya Yamaguchi <kyoryu32@gmail.com>
2021-05-14 22:52:56 +0900

    Initial Commit

---
+++ Nabeatsu/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Nabeatsu
+//
+//  Created by nagata on 2017/06/19.
+//  Copyright © 2017年 Life is Tech!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var faceLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func isAho() -> Bool {
+        number % 3 == 0 || "\(number)".contains("3")
+    }
+    @IBAction func plusButton(){
+        number = number + 1
+        countLabel.text = String(number)
+        
+        if isAho() == true {
+            
+            faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        } else {
+            
+            faceLabel.text = "(゜o゜)"
+        }
+    }
+
+
+}
+
